20873a603b338291b86b076ff9040c15cde17bee
Pantazis Deligiannis <pdeligia@microsoft.com>
2019-10-01 16:41:00 +0000

    Merged PR 580: Updated license text
    Following guidelines from https://docs.opensource.microsoft.com/releasing/copyright-headers.html
    
    Related work items: #867

--- Tools/Testing/Tester/Testing/TestingPortfolio.cs
+++ Tools/Testing/Tester/Testing/TestingPortfolio.cs
@@ -1,7 +1,5 @@
-﻿// ------------------------------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
-// ------------------------------------------------------------------------------------------------
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 using Microsoft.Coyote.Utilities;
 

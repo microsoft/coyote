4552368f3c2e5b8469af5a08a06aba5d77a4f755
Chris Lovett <clovett@microsoft.com>
2020-03-02 18:42:15 +0000

    Merged PR 1368: Fix website TOC and related bugs
    - Increase size of "small screen" to 640 so we can test it on a desktop using Chrome (desktop web browsers have a minimum width that is pretty big these days).
    - fix responsive intent of reference content and footer when "fixed" TOC is also auto-sizing itself.
    - Change scrollable TOC to "auto" so scrollbar is not always visible.
    - Make "scrollintoview" of selected TOC item jump to content on the phone sized screens where TOC is not a separate scrollable panel.
    - Fix auto-height setting of TOC so it does not do that on phone sized devices where TOC is not a "fixed" layout.
    - Fix height calculation of "fixed" layout TOC so it is not sensitive to page scroll position.

--- docs/assets/js/navmenu.js
+++ docs/assets/js/navmenu.js
@@ -16,33 +16,57 @@ function expand_nav(item)
         $(id).addClass("in");
     }
 
-    // make sure selected item is visible in the TOC by scrolling it into view.
-    item[0].scrollIntoView();
+    // make sure selected item is visible in the TOC by scrolling it into view
+    // unless the TOC is not scrollable (i.e. not "fixed") in which case we scroll
+    // the main reference content into view instead
+    var wrapper = $(".navmenu-fixed-left-wrapper");
+    if (wrapper.length && wrapper.css("position") == "fixed")
+    {
+        item[0].scrollIntoView();
+    }
+    else
+    {
+        // on the phone this will jump us from selected TOC entry down to the
+        // readable content on that topic.
+        var content = $(".reference-content");
+        if (content.length)
+        {
+            content[0].scrollIntoView();
+        }
+    }
 }
 
 function handle_resize()
 {
-    height = window.innerHeight;
-    wrapper = $(".nav-left-wrapper")
-    if (wrapper.length) {
-        var top = $(".nav-left-wrapper").offset().top
-        $(".nav-left-wrapper").height(height - top)
-    } else {
-        $(".navmenu").height(height);
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    var wrapper = $(".navmenu-fixed-left-wrapper");
+    if (wrapper.length && width >= 640)
+    {
+        var wrapper = $(".nav-left-wrapper")
+        if (wrapper.length) {
+            var search_top = $(".search-wrapper").offset().top;
+            var top = $(".nav-left-wrapper").offset().top;
+            var search_height = 110 + top - search_top;
+            var scroller_height = height - search_height
+            $(".nav-left-wrapper").height(scroller_height)
+        } else {
+            $(".navmenu").height(height);
+        }
     }
 }
 
-jQuery(document).ready(function ($) {
+$(document).ready(function () {
     $(window).resize(function(){
         handle_resize();
     });
     handle_resize();
 
     $(".navmenu").each(function() {
-        permalink = $(this).attr("permalink");
+        var permalink = $(this).attr("permalink");
         if (permalink.length){
             // ok we have one, so time to synchronize the expand/collapse state on the TOC.
-            li = $("li").find("[permalink='" + permalink + "']");
+            var li = $("li").find("[permalink='" + permalink + "']");
             expand_nav(li);
         }
     });

9630e4b2699d23420cd125cb14c9526d07de5ed5
Pantazis Deligiannis <pdeligia@microsoft.com>
2020-10-13 16:26:34 -0700

    edits

---
+++ Tests/BinaryRewriting.Tests/Invocations/UncontrolledTaskTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.IO;
+using System.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Coyote.BinaryRewriting.Tests.Tasks
+{
+    public class UncontrolledTaskTests : BaseProductionTest
+    {
+        public UncontrolledTaskTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestNotSupportedContinueWithTaskInvocation()
+        {
+            this.TestWithError(async () =>
+            {
+                string path = Assembly.GetExecutingAssembly().Location;
+                await File.ReadAllBytesAsync(path);
+            },
+            configuration: GetConfiguration().WithTestingIterations(100),
+            errorChecker: (e) =>
+            {
+                Assert.True(e.StartsWith("Method 'System.IO.File.ReadAllBytesAsync' returned an uncontrolled task"),
+                    "Expected uncontrolled task from invoking the async method.");
+            },
+            replay: true);
+        }
+    }
+}

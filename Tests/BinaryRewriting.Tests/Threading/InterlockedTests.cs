90bc105c0e5014123f5f78d619835adee7db9e69
Udit kumar agarwal <16324601+madaari@users.noreply.github.com>
2020-10-20 20:32:14 +0530

    Fixed failing InterlockedTests.

--- Tests/BinaryRewriting.Tests/Threading/InterlockedTests.cs
+++ Tests/BinaryRewriting.Tests/Threading/InterlockedTests.cs
@@ -19,34 +19,36 @@ namespace Microsoft.Coyote.BinaryRewriting.Tests.Threading.Tasks
         [Fact(Timeout = 5000)]
         public void TestInterlockedIncrement()
         {
-            this.Test(async () =>
+            this.TestWithError(async () =>
             {
                 SharedEntry entry = new SharedEntry();
                 entry.Value = 0;
 
-                Task task1 = Task.Run( () =>
-                {
-                    Interlocked.Exchange(ref entry.Value, 0);
-                    Interlocked.Increment(ref entry.Value);
-                });
+                Task task1 = Task.Run(() =>
+               {
+                   Interlocked.Exchange(ref entry.Value, 0);
+                   Interlocked.Increment(ref entry.Value);
+               });
 
-                Task task2 = Task.Run( () =>
-                {
-                    Interlocked.Exchange(ref entry.Value, 0);
-                    Interlocked.Increment(ref entry.Value);
-                });
+                Task task2 = Task.Run(() =>
+               {
+                   Interlocked.Exchange(ref entry.Value, 0);
+                   Interlocked.Increment(ref entry.Value);
+               });
 
                 await task1;
                 await task2;
                 Specification.Assert(entry.Value == 1, $"Value is {entry.Value} instead of 1.");
             },
-            configuration: GetConfiguration().WithTestingIterations(500));
+            configuration: GetConfiguration().WithTestingIterations(500),
+            replay: true,
+            expectedError: "Value is 2 instead of 1.");
         }
 
         [Fact(Timeout = 5000)]
         public void TestInterlockedDecrement()
         {
-            this.Test(async () =>
+            this.TestWithError(async () =>
             {
                 SharedEntry entry = new SharedEntry();
                 entry.Value = 1;
@@ -67,7 +69,9 @@ namespace Microsoft.Coyote.BinaryRewriting.Tests.Threading.Tasks
                 await task2;
                 Specification.Assert(entry.Value == 0, $"Value is {entry.Value} instead of 0.");
             },
-            configuration: GetConfiguration().WithTestingIterations(500));
+            configuration: GetConfiguration().WithTestingIterations(500),
+            replay: true,
+            expectedError: "Value is -1 instead of 0.");
         }
     }
 }

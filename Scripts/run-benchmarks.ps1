ff84f4ffc6bba6cb15c683bb14860f0f936ca9c8
Pantazis Deligiannis <pdeligia@microsoft.com>
2020-10-26 08:25:56 -0700

    edits

--- Scripts/run-benchmarks.ps1
+++ Scripts/run-benchmarks.ps1
@@ -1,60 +1,29 @@
 param(
-    [string]$store = "",
-    [string]$key = "",
-    [string]$local = ""
+    [boolean]$set_vso_result_var = $false
 )
 
-Import-Module $PSScriptRoot\powershell\common.psm1 -Force
-
-$history = Invoke-Expression "git log --pretty=oneline -n 1"
-$words = $history.Split(' ')
-$commit = $words[0]
-if ($store -ne "") {
-    $env:AZURE_COSMOSDB_ENDPOINT = $store
-    $env:AZURE_STORAGE_PRIMARY_KEY = $key
-    Write-Host "Results will be saved to $ENV:AZURE_COSMOSDB_ENDPOINT"
-    $cosmos = "-cosmos"
-}
-else {
-    $cosmos = ""
-}
+Import-Module $PSScriptRoot\powershell\common.psm1
 
 $current_dir = (Get-Item -Path ".\").FullName
 $benchmarks_dir = "$PSScriptRoot\..\Tools\BenchmarkRunner\bin\netcoreapp3.1"
 $benchmark_runner = "BenchmarkRunner.exe"
-$artifacts_dir = "$current_dir\benchmark_$commit"
+$artifacts_dir = "$current_dir\BenchmarkDotNet.Artifacts"
+$timestamp = (Get-Date).ToString('yyyy_MM_dd_hh_mm_ss')
+$results = "benchmark_results_$timestamp"
 
-if (-Not (Test-Path -Path "$benchmarks_dir")) {
-    throw "Please build coyote project first"
+Write-Comment -prefix "." -text "Running the Coyote performance benchmarks" -color "yellow"
+
+Invoke-Expression "$benchmarks_dir\$benchmark_runner"
+
+if (-not (Test-Path $artifacts_dir)) {
+    Write-Error "Unable to find the benchmark results ($artifacts_dir)."
+    exit
 }
 
-$custom = "D:\git\lovettchris\BenchmarkDotNet\src\BenchmarkDotNet\bin\Release\netstandard2.0"
-if (Test-Path -Path $custom) {
-    Write-Host "==> Using a patched version of BenchmarkDotNet..."
-    Copy-Item "$custom\BenchmarkDotNet.dll" "$benchmarks_dir\BenchmarkDotNet.dll" -Force
-    Copy-Item "$custom\BenchmarkDotNet.Annotations.dll" "$benchmarks_dir\BenchmarkDotNet.Annotations.dll" -Force
+Rename-Item -path $artifacts_dir -newName "$results"
+
+if ($set_vso_result_var) {
+    Write-Host "##vso[task.setvariable variable=vso_benchmark_results;]$current_dir\$results"
 }
-
-if (Test-Path -Path $artifacts_dir -PathType Container) {
-    Remove-Item $artifacts_dir -Recurse
-}
-
-Write-Comment -prefix "." -text "Running the Coyote performance benchmarks, saving to $artifacts_dir" -color "yellow"
-
-Invoke-Expression "$benchmarks_dir\$benchmark_runner -outdir $artifacts_dir -commit $commit $cosmos"
 
 Write-Comment -prefix "." -text "Done" -color "green"
-
-if ($local -ne "") {
-    # save the detailed perf results on the test machine with additional integer index to
-    # disambiguate duplicate runs for the same commit id.
-    if (-not (Test-Path -Path $local)) {
-        New-Item -Path $local -ItemType Directory
-    }
-    $index = 1
-    while (Test-Path -Path "$local\benchmark_$commit.$index") {
-        $index = $index + 1
-    }
-
-    Move-Item -Path $artifacts_dir -Destination "$local\benchmark_$commit.$index"
-}

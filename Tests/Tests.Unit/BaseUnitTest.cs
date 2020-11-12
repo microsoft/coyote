c87bca315bee37c3836a72fd495b5e04a3fac81f
Pantazis Deligiannis <pdeligia@microsoft.com>
2020-11-12 08:26:17 -0800

    edits

---
+++ Tests/Tests.Unit/BaseUnitTest.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.Coyote.Tests.Common;
+using Xunit.Abstractions;
+
+namespace Microsoft.Coyote.Tests
+{
+    public abstract class BaseUnitTest : BaseTest
+    {
+        public BaseUnitTest(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        protected class SharedEntry
+        {
+            public volatile int Value = 0;
+
+            public async Task<int> GetWriteResultAsync(int value)
+            {
+                this.Value = value;
+                await Task.CompletedTask;
+                return this.Value;
+            }
+
+            public async Task<int> GetWriteResultWithDelayAsync(int value)
+            {
+                this.Value = value;
+                await Task.Delay(5);
+                return this.Value;
+            }
+        }
+    }
+}

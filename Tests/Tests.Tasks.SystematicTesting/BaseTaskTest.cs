8229d9138892f580163757fa9d9f581c69534314
Pantazis Deligiannis <pdeligia@microsoft.com>
2020-11-12 07:54:20 -0800

    renaming

---
+++ Tests/Tests.Tasks.SystematicTesting/BaseTaskTest.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Coyote.Specifications;
+using Microsoft.Coyote.Tests.Common;
+using Xunit.Abstractions;
+
+namespace Microsoft.Coyote.Tasks.SystematicTesting.Tests
+{
+    public abstract class BaseTaskTest : BaseTest
+    {
+        public BaseTaskTest(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        protected override bool IsSystematicTest => true;
+
+        protected static void AssertSharedEntryValue(SharedEntry entry, int expected) =>
+            Specification.Assert(entry.Value == expected, "Value is {0} instead of {1}.", entry.Value, expected);
+
+        protected class SharedEntry
+        {
+            public volatile int Value = 0;
+
+            public async Task<int> GetWriteResultAsync(int value)
+            {
+                this.Value = value;
+                await Task.CompletedTask;
+                return this.Value;
+            }
+
+            public async Task<int> GetWriteResultWithDelayAsync(int value)
+            {
+                this.Value = value;
+                await Task.Delay(1);
+                return this.Value;
+            }
+        }
+    }
+}
---
+++ Tests/Tests.Tasks.SystematicTesting/RandomValues/TaskRandomIntegerTests.cs
@@ -0,0 +1,190 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Coyote.Random;
+using Microsoft.Coyote.Specifications;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Coyote.Tasks.SystematicTesting.Tests
+{
+    public class TaskRandomIntegerTests : BaseTaskTest
+    {
+        public TaskRandomIntegerTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestRandomIntegerInSynchronousTask()
+        {
+            this.TestWithError(async () =>
+            {
+                Generator generator = Generator.Create();
+                SharedEntry entry = new SharedEntry();
+
+                async Task WriteAsync()
+                {
+                    await Task.CompletedTask;
+                    if (generator.NextInteger(5) is 0)
+                    {
+                        entry.Value = 3;
+                    }
+                    else
+                    {
+                        entry.Value = 5;
+                    }
+                }
+
+                await WriteAsync();
+                AssertSharedEntryValue(entry, 5);
+            },
+            configuration: GetConfiguration().WithTestingIterations(200),
+            expectedError: "Value is 3 instead of 5.",
+            replay: true);
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestRandomIntegerInAsynchronousTask()
+        {
+            this.TestWithError(async () =>
+            {
+                Generator generator = Generator.Create();
+                SharedEntry entry = new SharedEntry();
+
+                async Task WriteWithDelayAsync()
+                {
+                    await Task.Delay(1);
+                    if (generator.NextInteger(5) is 0)
+                    {
+                        entry.Value = 3;
+                    }
+                    else
+                    {
+                        entry.Value = 5;
+                    }
+                }
+
+                await WriteWithDelayAsync();
+                AssertSharedEntryValue(entry, 5);
+            },
+            configuration: GetConfiguration().WithTestingIterations(200),
+            expectedError: "Value is 3 instead of 5.",
+            replay: true);
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestRandomIntegerInParallelTask()
+        {
+            this.TestWithError(async () =>
+            {
+                Generator generator = Generator.Create();
+                SharedEntry entry = new SharedEntry();
+
+                await Task.Run(() =>
+                {
+                    if (generator.NextInteger(5) is 0)
+                    {
+                        entry.Value = 3;
+                    }
+                    else
+                    {
+                        entry.Value = 5;
+                    }
+                });
+
+                AssertSharedEntryValue(entry, 5);
+            },
+            configuration: GetConfiguration().WithTestingIterations(200),
+            expectedError: "Value is 3 instead of 5.",
+            replay: true);
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestRandomIntegerInParallelSynchronousTask()
+        {
+            this.TestWithError(async () =>
+            {
+                Generator generator = Generator.Create();
+                SharedEntry entry = new SharedEntry();
+
+                await Task.Run(async () =>
+                {
+                    await Task.CompletedTask;
+                    if (generator.NextInteger(5) is 0)
+                    {
+                        entry.Value = 3;
+                    }
+                    else
+                    {
+                        entry.Value = 5;
+                    }
+                });
+
+                AssertSharedEntryValue(entry, 5);
+            },
+            configuration: GetConfiguration().WithTestingIterations(200),
+            expectedError: "Value is 3 instead of 5.",
+            replay: true);
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestRandomIntegerInParallelAsynchronousTask()
+        {
+            this.TestWithError(async () =>
+            {
+                Generator generator = Generator.Create();
+                SharedEntry entry = new SharedEntry();
+
+                await Task.Run(async () =>
+                {
+                    await Task.Delay(1);
+                    if (generator.NextInteger(5) is 0)
+                    {
+                        entry.Value = 3;
+                    }
+                    else
+                    {
+                        entry.Value = 5;
+                    }
+                });
+
+                AssertSharedEntryValue(entry, 5);
+            },
+            configuration: GetConfiguration().WithTestingIterations(200),
+            expectedError: "Value is 3 instead of 5.",
+            replay: true);
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestRandomIntegerInNestedParallelSynchronousTask()
+        {
+            this.TestWithError(async () =>
+            {
+                Generator generator = Generator.Create();
+                SharedEntry entry = new SharedEntry();
+
+                await Task.Run(async () =>
+                {
+                    await Task.Run(async () =>
+                    {
+                        await Task.CompletedTask;
+                        if (generator.NextInteger(5) is 0)
+                        {
+                            entry.Value = 3;
+                        }
+                        else
+                        {
+                            entry.Value = 5;
+                        }
+                    });
+                });
+
+                AssertSharedEntryValue(entry, 5);
+            },
+            configuration: GetConfiguration().WithTestingIterations(200),
+            expectedError: "Value is 3 instead of 5.",
+            replay: true);
+        }
+    }
+}

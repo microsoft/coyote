6a483c4fcdaa890ed4f4017d845e08bea58e6042
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-03-31 11:08:51 -0700

    edit

--- Tests/Tests.Common/BaseTest.cs
+++ Tests/Tests.Common/BaseTest.cs
@@ -280,7 +280,7 @@ namespace Microsoft.Coyote.Tests.Common
             if (this.SchedulingPolicy is SchedulingPolicy.Fuzzing)
             {
                 // Increase iterations during fuzzing as some bugs might be harder to be found.
-                configuration = configuration.WithTestingIterations(configuration.TestingIterations * 10);
+                configuration = configuration.WithTestingIterations(configuration.TestingIterations * 20);
             }
 
             ILogger logger = this.GetLogger(configuration);
@@ -372,7 +372,7 @@ namespace Microsoft.Coyote.Tests.Common
             if (this.SchedulingPolicy is SchedulingPolicy.Fuzzing)
             {
                 // Increase iterations during fuzzing as some bugs might be harder to be found.
-                configuration = configuration.WithTestingIterations(configuration.TestingIterations * 10);
+                configuration = configuration.WithTestingIterations(configuration.TestingIterations * 20);
             }
 
             Type exceptionType = typeof(TException);

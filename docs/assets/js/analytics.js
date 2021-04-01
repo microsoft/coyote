d6a424ff17dcb5c67963e91c2af647cae9b9739a
Chris Lovett <clovett@microsoft.com>
2021-04-01 14:07:33 -0700

    set google analytics cookie expiration to 1 year.

--- docs/assets/js/analytics.js
+++ docs/assets/js/analytics.js
@@ -10,15 +10,21 @@ function enableTelemetry() {
 
         // enable google analytics.
         window['ga-disable-UA-161403370-1'] = false;
-
+        
         // see https://developers.google.com/analytics/devguides/collection/analyticsjs/
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-161403370-1', 'auto');
+        var year = 365 * 24 * 60 * 60; // 1 year in seconds
+        var flags = 'max-age=' + year + ';secure;samesite=none';
+        ga('create', 'UA-161403370-1', {
+            cookieFlags: flags
+          });
         ga('send', 'pageview');
+
+        console.log("Recording pageview on " + window.location.href);
     }
 }
 
@@ -60,6 +66,17 @@ function manageCookies() {
       });
 }
 
-$(document).ready(function () {
+function initAnalytics() {
     WcpConsent.init("en-US", "cookie-banner", wcp_ready, onConsentChanged);
+}
+
+$(document).ready(function () {
+    // $(document).ready triggers twice, once for the outer TOC and again for the iframe.
+    // We only want one pageview event so we trigger on iframe since it has the
+    // correct href location and not the funky '#' page reference which the TOC produces
+    // as that would record everything as hitting the home page.
+    if (window.top != window.self) {
+        telemetryInitialized = false;
+        initAnalytics();
+    };
 });

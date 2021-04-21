30985ea4613508eeeb3d643ea89b07105fbaa0a6
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-04-20 18:50:43 -0700

    edits

--- Tests/Tests.BugFinding/DataRaceChecking/GenericListTests.cs
+++ Tests/Tests.BugFinding/DataRaceChecking/GenericListTests.cs
@@ -16,7 +16,7 @@ namespace Microsoft.Coyote.BugFinding.Tests.DataRaceChecking
         }
 
         [Fact(Timeout = 5000)]
-        public void TestGenericListDataRace()
+        public void TestGenericListAddDataRace()
         {
             this.TestWithError(async () =>
             {
@@ -30,6 +30,82 @@ namespace Microsoft.Coyote.BugFinding.Tests.DataRaceChecking
                 Task t2 = Task.Run(() =>
                 {
                     list.Add(2);
+                });
+
+                await Task.WhenAll(t1, t2);
+            },
+            configuration: this.GetConfiguration().WithTestingIterations(100),
+            expectedError: $"Found write/write data race on '{typeof(List<int>)}'.",
+            replay: true);
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestGenericListIndex()
+        {
+            this.Test(async () =>
+            {
+                var list = new List<int>
+                {
+                    1
+                };
+
+                Task t1 = Task.Run(() =>
+                {
+                    _ = list[0];
+                });
+
+                Task t2 = Task.Run(() =>
+                {
+                    _ = list[0];
+                });
+
+                await Task.WhenAll(t1, t2);
+            },
+            configuration: this.GetConfiguration().WithTestingIterations(100));
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestGenericListIndexDataRace()
+        {
+            this.TestWithError(async () =>
+            {
+                var list = new List<int>
+                {
+                    1
+                };
+
+                Task t1 = Task.Run(() =>
+                {
+                    _ = list[0];
+                });
+
+                Task t2 = Task.Run(() =>
+                {
+                    list[0] = 2;
+                });
+
+                await Task.WhenAll(t1, t2);
+            },
+            configuration: this.GetConfiguration().WithTestingIterations(100),
+            expectedError: $"Found read/write data race on '{typeof(List<int>)}'.",
+            replay: true);
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestGenericListCapacityDataRace()
+        {
+            this.TestWithError(async () =>
+            {
+                var list = new List<int>();
+
+                Task t1 = Task.Run(() =>
+                {
+                    list.Capacity = 2;
+                });
+
+                Task t2 = Task.Run(() =>
+                {
+                    list.Capacity = 5;
                 });
 
                 await Task.WhenAll(t1, t2);

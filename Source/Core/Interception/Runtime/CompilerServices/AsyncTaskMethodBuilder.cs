537545c3f0c8622c2dc158d26b7447b8acdfdf56
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-10-06 15:37:36 -0700

    fix

--- Source/Core/Interception/Runtime/CompilerServices/AsyncTaskMethodBuilder.cs
+++ Source/Core/Interception/Runtime/CompilerServices/AsyncTaskMethodBuilder.cs
@@ -5,7 +5,6 @@ using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Coyote.Runtime;
 using SystemCompiler = System.Runtime.CompilerServices;

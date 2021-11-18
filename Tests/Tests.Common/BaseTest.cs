d8a23175b32597217b5c3a43ca20f3a713296709
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-11-18 11:23:06 -0800

    fix

--- Tests/Tests.Common/BaseTest.cs
+++ Tests/Tests.Common/BaseTest.cs
@@ -11,6 +11,7 @@ using Microsoft.Coyote.Actors;
 using Microsoft.Coyote.Actors.Coverage;
 using Microsoft.Coyote.IO;
 using Microsoft.Coyote.Runtime;
+using Microsoft.Coyote.SystematicTesting;
 using Xunit;
 using Xunit.Abstractions;
 using ActorRuntimeFactory = Microsoft.Coyote.Actors.RuntimeFactory;

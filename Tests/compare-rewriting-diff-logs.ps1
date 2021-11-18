c034360ccf674ece44deef840bfc7cc5953a4add
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-11-17 18:23:13 -0800

    fixes

--- Tests/compare-rewriting-diff-logs.ps1
+++ Tests/compare-rewriting-diff-logs.ps1
@@ -6,10 +6,10 @@ Import-Module $PSScriptRoot/../Scripts/powershell/common.psm1 -Force
 $framework = "net5.0"
 $targets = [ordered]@{
     "rewriting" = "Tests.Rewriting"
+    "rewriting-helpers" = "Tests.Rewriting.Helpers"
     "testing" = "Tests.BugFinding"
     "actors" = "Tests.Actors"
     "actors-testing" = "Tests.Actors.BugFinding"
-    "standalone" = "Tests.Standalone"
 }
 
 $expected_hashes = [ordered]@{
@@ -17,7 +17,7 @@ $expected_hashes = [ordered]@{
     "testing" = "FF77454582428126712A51C8EAF910444505DA38BC0BE56659CF3BDD9E8845F5"
     "actors" = "38744E42DDD648ED448ABDF083A24875F2812C0A6FB350C121B916665BC1E9E9"
     "actors-testing" = "B6898714431A66E4C1C4D6A3F978BD25906374F8E54D27D7924D4BDF2424F8D9"
-    "standalone" = "FB0286E1172EFCD94E51E392829BCAADD7E32F7834D71F109FACC155A48BB030"
+    "rewriting-helpers" = "FB0286E1172EFCD94E51E392829BCAADD7E32F7834D71F109FACC155A48BB030"
 }
 
 Write-Comment -prefix "." -text "Comparing the test rewriting diff logs" -color "yellow"

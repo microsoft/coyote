d82fcd93bf5cba5c582bdfa12d49798dc7cb286b
Pantazis Deligiannis <pdeligia@microsoft.com>
2022-03-18 09:19:12 -0700

    new tests

---
+++ Tests/Tests.BugFinding/PartialControl/UncontrolledDeadlockTests.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Coyote.BugFinding.Tests
+{
+    public class UncontrolledDeadlockTests : BaseBugFindingTest
+    {
+        public UncontrolledDeadlockTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestUncontrolledDeadlock()
+        {
+            this.TestWithError(async () =>
+            {
+                SemaphoreSlim semaphore = new SemaphoreSlim(1);
+                Task task = Task.Run(async () =>
+                {
+                    semaphore.Wait(100);
+                    await Task.Delay(1);
+                    semaphore.Release();
+                });
+
+                semaphore.Wait(100);
+                await Task.Delay(1);
+                semaphore.Release();
+                await task;
+            },
+            configuration: this.GetConfiguration()
+                .WithPartiallyControlledConcurrencyAllowed()
+                .WithDeadlockTimeout(100)
+                .WithTestingIterations(10),
+            errorChecker: (e) =>
+            {
+                Assert.StartsWith($"Potential deadlock detected. Because a deadlock detection timeout", e);
+            });
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestUncontrolledDeadlockReportedAsNoBug()
+        {
+            this.Test(async () =>
+            {
+                SemaphoreSlim semaphore = new SemaphoreSlim(1);
+                Task task = Task.Run(async () =>
+                {
+                    semaphore.Wait(100);
+                    await Task.Delay(1);
+                    semaphore.Release();
+                });
+
+                semaphore.Wait(100);
+                await Task.Delay(1);
+                semaphore.Release();
+                await task;
+            },
+            configuration: this.GetConfiguration()
+                .WithPartiallyControlledConcurrencyAllowed()
+                .WithPotentialDeadlocksReportedAsBugs(false)
+                .WithDeadlockTimeout(10)
+                .WithTestingIterations(10));
+        }
+    }
+}

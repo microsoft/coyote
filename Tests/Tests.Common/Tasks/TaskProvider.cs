9b844ef39ae3093a0ab3f59fe3fa9db55cf19bef
Pantazis Deligiannis <pdeligia@microsoft.com>
2022-03-21 18:53:51 -0700

    fix

--- Tests/Tests.Common/Tasks/TaskProvider.cs
+++ Tests/Tests.Common/Tasks/TaskProvider.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Coyote.Tests.Common.Tasks
 {
+#pragma warning disable CA1000 // Do not declare static members on generic types
     /// <summary>
     /// Helper class for task rewriting tests.
     /// </summary>
     /// <remarks>
     /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
     /// </remarks>
-    public class TaskProvider
+    public static class TaskProvider
     {
         public static Task GetTask() => Task.CompletedTask;
 
@@ -24,15 +25,15 @@ namespace Microsoft.Coyote.Tests.Common.Tasks
     /// <remarks>
     /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
     /// </remarks>
-    public class GenericTaskProvider<K,Z>
+    public static class GenericTaskProvider<TLeft, TRight>
     {
-        public class Nested<L>
+        public static class Nested<TNested>
         {
             public static Task GetTask() => Task.CompletedTask;
 
             public static Task<T> GetGenericMethodTask<T>() => Task.FromResult<T>(default(T));
 
-            public static Task<Z> GetGenericTypeTask<T>() => Task.FromResult<Z>(default(Z));
+            public static Task<TRight> GetGenericTypeTask<T>() => Task.FromResult<TRight>(default(TRight));
         }
     }
 
@@ -42,7 +43,7 @@ namespace Microsoft.Coyote.Tests.Common.Tasks
     /// <remarks>
     /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
     /// </remarks>
-    public class ValueTaskProvider
+    public static class ValueTaskProvider
     {
         public static ValueTask GetTask() => ValueTask.CompletedTask;
 
@@ -55,15 +56,16 @@ namespace Microsoft.Coyote.Tests.Common.Tasks
     /// <remarks>
     /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
     /// </remarks>
-    public class GenericValueTaskProvider<K,Z>
+    public static class GenericValueTaskProvider<TLeft, TRight>
     {
-        public class Nested<L>
+        public static class Nested<TNested>
         {
             public static ValueTask GetTask() => ValueTask.CompletedTask;
 
             public static ValueTask<T> GetGenericMethodTask<T>() => ValueTask.FromResult<T>(default(T));
 
-            public static ValueTask<Z> GetGenericTypeTask<T>() => ValueTask.FromResult<Z>(default(Z));
+            public static ValueTask<TRight> GetGenericTypeTask<T>() => ValueTask.FromResult<TRight>(default(TRight));
         }
     }
+#pragma warning restore CA1000 // Do not declare static members on generic types
 }

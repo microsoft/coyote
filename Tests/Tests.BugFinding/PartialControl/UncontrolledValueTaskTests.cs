f80fcccf1b3a32ef05eff7cd1ad9d48cfc60cc81
Pantazis Deligiannis <pdeligia@microsoft.com>
2023-06-05 10:26:56 -0700

    tests

--- Tests/Tests.BugFinding/PartialControl/UncontrolledValueTaskTests.cs
+++ Tests/Tests.BugFinding/PartialControl/UncontrolledValueTaskTests.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using Microsoft.Coyote.Rewriting;
+using Microsoft.Coyote.Tests.Common.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -14,52 +12,6 @@ namespace Microsoft.Coyote.BugFinding.Tests
         public UncontrolledValueTaskTests(ITestOutputHelper output)
             : base(output)
         {
-        }
-
-        /// <summary>
-        /// Helper class for task rewriting tests.
-        /// </summary>
-        /// <remarks>
-        /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
-        /// </remarks>
-        [SkipRewriting("Must not be rewritten.")]
-        private class UncontrolledValueTaskAwaiter
-        {
-#if NET
-#pragma warning disable CA1822 // Mark members as static
-            public ValueTaskAwaiter GetAwaiter() => ValueTask.CompletedTask.GetAwaiter();
-#pragma warning restore CA1822 // Mark members as static
-#endif
-        }
-
-        /// <summary>
-        /// Helper class for task rewriting tests.
-        /// </summary>
-        /// <remarks>
-        /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
-        /// </remarks>
-        [SkipRewriting("Must not be rewritten.")]
-        private class UncontrolledGenericValueTaskAwaiter
-        {
-#if NET
-#pragma warning disable CA1822 // Mark members as static
-            public ValueTaskAwaiter<int> GetAwaiter() => ValueTask.FromResult<int>(0).GetAwaiter();
-#pragma warning restore CA1822 // Mark members as static
-#endif
-        }
-
-        /// <summary>
-        /// Helper class for task rewriting tests.
-        /// </summary>
-        /// <remarks>
-        /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
-        /// </remarks>
-        [SkipRewriting("Must not be rewritten.")]
-        private class UncontrolledValueTaskAwaiter<T>
-        {
-#if NET
-            public ValueTaskAwaiter<T> GetAwaiter() => ValueTask.FromResult<T>(default).GetAwaiter();
-#endif
         }
 
 #if NET
---
+++ Tests/Tests.Common/Tasks/UncontrolledValueTaskAwaiter.cs
@@ -0,0 +1,51 @@
+﻿﻿﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Microsoft.Coyote.Tests.Common.Tasks
+{
+    /// <summary>
+    /// Helper class for task rewriting tests.
+    /// </summary>
+    /// <remarks>
+    /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
+    /// </remarks>
+    public class UncontrolledValueTaskAwaiter
+    {
+#if NET
+#pragma warning disable CA1822 // Mark members as static
+        public ValueTaskAwaiter GetAwaiter() => ValueTask.CompletedTask.GetAwaiter();
+#pragma warning restore CA1822 // Mark members as static
+#endif
+    }
+
+    /// <summary>
+    /// Helper class for task rewriting tests.
+    /// </summary>
+    /// <remarks>
+    /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
+    /// </remarks>
+    public class UncontrolledGenericValueTaskAwaiter
+    {
+#if NET
+#pragma warning disable CA1822 // Mark members as static
+        public ValueTaskAwaiter<int> GetAwaiter() => ValueTask.FromResult<int>(0).GetAwaiter();
+#pragma warning restore CA1822 // Mark members as static
+#endif
+    }
+
+    /// <summary>
+    /// Helper class for task rewriting tests.
+    /// </summary>
+    /// <remarks>
+    /// We do not rewrite this class in purpose to test scenarios with partially rewritten code.
+    /// </remarks>
+    public class UncontrolledValueTaskAwaiter<T>
+    {
+#if NET
+        public ValueTaskAwaiter<T> GetAwaiter() => ValueTask.FromResult<T>(default).GetAwaiter();
+#endif
+    }
+}
